85a2cfef45eec4cd078f1bb7d97c502816b39866
Jheitor012 <jhpagotto@hotmail.com>
2021-03-02 16:49:20 -0300

    more recent angular project version

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('landing-page-type-one app is running!');
+    expect(page.getTitleText()).toEqual('angular-productive app is running!');
   });
 
   afterEach(async () => {
