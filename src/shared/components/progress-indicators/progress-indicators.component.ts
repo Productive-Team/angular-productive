b332e32ef6018e2c9332a85df521bdb9ec75c8ff
Jheitor012 <jhpagotto@hotmail.com>
2021-05-04 15:12:47 -0300

    started working on loading bar

--- src/shared/components/progress-indicators/progress-indicators.component.ts
+++ src/shared/components/progress-indicators/progress-indicators.component.ts
@@ -11,13 +11,27 @@ import {
   templateUrl: './progress-indicators.component.html',
   styleUrls: ['./progress-indicators.component.css'],
 })
-export class ProgressIndicatorsComponent {
+export class ProgressIndicatorsComponent implements OnInit {
   @Input() pProgressType = 'spinner';
   @Input() pProgressColor = 'var(--primary)';
-  @Input() pProgressStatus: string;
+  @Input() pProgressDeterminate = true;
   @Input() pProgressSize = 0;
   // @Input() pSpinnerProgress: number;
   // @Input() pBarProgress: number;
 
   constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      if (!this.pProgressDeterminate) {
+        const el = this.el.nativeElement.firstChild.firstChild
+          .firstChild as HTMLDivElement;
+        el.classList.add('animated');
+        if (this.pProgressType === 'spinner') {
+          const el2 = this.el.nativeElement.firstChild as HTMLDivElement;
+          el2.classList.add('rotate');
+        }
+      }
+    }, 0);
+  }
 }
