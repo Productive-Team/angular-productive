7e0bf8026a5bd3ec9c8153b8727e39f63c1404a6
Jose Heitor Pagotto <jhpagotto@hotmail.com>
2021-07-23 18:03:55 -0300

    refactored tooltips positioning

--- src/shared/directives/tooltips/tooltips.directive.ts
+++ src/shared/directives/tooltips/tooltips.directive.ts
@@ -9,9 +9,7 @@ export class TooltipsDirective {
   @Input() pTooltipClickOpen = false;
   @Input() pTooltipClickDuration = 2500;
 
-  constructor(private el: ElementRef) {
-    // this.insertElementIntoContainer(this.el.nativeElement);
-  }
+  constructor(private el: ElementRef) {}
 
   @HostListener('mouseover', ['$event']) showTooltip(event): void {
     const allTooltips = document.querySelectorAll('.pTooltip');
@@ -24,7 +22,7 @@ export class TooltipsDirective {
 
   @HostListener('mouseout', ['$event']) hideTooltip(event): void {
     if (!this.pTooltipClickOpen) {
-      // this.removeTooltip();
+      this.removeTooltip();
     }
   }
 
@@ -32,10 +30,10 @@ export class TooltipsDirective {
     if (this.pTooltipClickOpen) {
       const allTooltips = document.querySelectorAll('.pTooltip');
       if (allTooltips.length < 1) {
-        this.createToolTip(event.target, this.pTooltipText);
+        this.createToolTip(this.el.nativeElement, this.pTooltipText);
         setTimeout(
           () => {
-            const tt = event.target.nextSibling;
+            const tt = allTooltips[0] as HTMLElement;
             tt.style.opacity = '0';
             tt.addEventListener('transitionend', () => {
               this.removeTooltip();
@@ -47,46 +45,64 @@ export class TooltipsDirective {
     }
   }
 
-  private insertElementIntoContainer(element: HTMLElement): void {
-    const container = document.createElement('div');
-    container.classList.add('p-tooltip-container');
-    element.insertAdjacentElement('beforebegin', container);
-    container.insertAdjacentElement('beforeend', element);
+  @HostListener('touchstart', ['$event'])
+  showTooltipMobile(event): void {
+    setTimeout(() => {
+      this.createToolTip(this.el.nativeElement, this.pTooltipText);
+    }, 500);
   }
 
   private setTooltipPosition(element: HTMLElement, tooltip: HTMLElement): void {
     const elPos = element.getBoundingClientRect();
-    const toolPos = tooltip.getBoundingClientRect();
+    tooltip.style.height = elPos.height + 'px';
     switch (this.pTooltipPosition) {
       case 'left':
-        // element.classList.add('left-align');
+        tooltip.style.top = elPos.top + 'px';
+        tooltip.style.left = elPos.left - elPos.width + 'px';
         break;
       case 'top':
-        tooltip.style.top = elPos.top - elPos.height / 2 - 5 + 'px';
-        tooltip.style.left = elPos.left + 'px';
-        console.log(elPos.left - elPos.right);
+        tooltip.style.top = elPos.top - elPos.height + 'px';
+        tooltip.style.left =
+          elPos.left + (element.offsetWidth - tooltip.offsetWidth) / 2 + 'px';
         break;
       case 'bottom':
-        tooltip.style.top = elPos.bottom + 5 + 'px';
+        tooltip.style.top = elPos.bottom + 'px';
         tooltip.style.left =
-          elPos.left - toolPos.width / 2 - elPos.width / 2 + 'px';
+          elPos.left + (element.offsetWidth - tooltip.offsetWidth) / 2 + 'px';
         break;
       case 'right':
-        // element.classList.add('right-align');
+        tooltip.style.top = elPos.top + 'px';
+        tooltip.style.left = elPos.right + 'px';
         break;
       default:
-        tooltip.style.top = elPos.bottom + 5 + 'px';
+        tooltip.style.top = elPos.bottom + 'px';
         tooltip.style.left =
-          elPos.left - toolPos.width / 2 - elPos.width / 2 + 'px';
+          elPos.left + (element.offsetWidth - tooltip.offsetWidth) / 2 + 'px';
+    }
+    const toolPos = tooltip.getBoundingClientRect();
+
+    if (toolPos.top + toolPos.height > window.innerHeight) {
+      tooltip.style.top = elPos.top - elPos.height + 'px';
+    } else if (toolPos.top < 0) {
+      tooltip.style.top = elPos.bottom + 'px';
+    }
+    if (toolPos.left + toolPos.width > window.innerWidth) {
+      tooltip.style.left = null;
+      tooltip.style.right = '1rem';
+    } else if (toolPos.left < 0) {
+      tooltip.style.left = '1rem';
     }
   }
 
   private createToolTip(element: HTMLElement, tooltipText: string): void {
+    const tooltipContainer = document.createElement('div');
+    tooltipContainer.classList.add('tooltip-container');
     const tooltip = document.createElement('span');
     tooltip.classList.add('pTooltip');
     tooltip.innerHTML = tooltipText;
-    document.body.insertAdjacentElement('beforeend', tooltip);
-    this.setTooltipPosition(element, tooltip);
+    tooltipContainer.insertAdjacentElement('beforeend', tooltip);
+    document.body.insertAdjacentElement('beforeend', tooltipContainer);
+    this.setTooltipPosition(element, tooltipContainer);
   }
 
   private removeTooltip(): any {
