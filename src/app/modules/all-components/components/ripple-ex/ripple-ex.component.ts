d02e7816508e01e068ba0966892d43720d845a7b
heitorpagotto <jhpagotto@hotmail.com>
2021-11-11 14:43:40 -0300

    added hljs styling

--- src/app/modules/all-components/components/ripple-ex/ripple-ex.component.ts
+++ src/app/modules/all-components/components/ripple-ex/ripple-ex.component.ts
@@ -17,17 +17,18 @@ export class RippleExComponent {
 
   showCodeSection2: boolean;
 
-  codeSection1 = `<div class="ripple-example elevation-p8" pRipple [pRippleTriggerFor]="rippleTrigger">Click Me!</div><div class="ripple-example elevation-p8" #rippleTrigger></div>`;
+  codeSection1 = `<div class="ripple-example elevation-p8" pRipple [pRippleTriggerFor]="rippleTrigger">Click Me!</div>
+ <div class="ripple-example elevation-p8" #rippleTrigger></div>`;
   codeSection2 = `.ripple-example {
-  margin: 15px 1rem;
-  width: 250px;
-  height: 250px;
-  line-height: 250px;
-  user-select: none;
-  cursor: pointer;
-  text-align: center;
-  -webkit-tap-highlight-color: transparent;
-}`;
+   margin: 15px 1rem;
+   width: 250px;
+   height: 250px;
+   line-height: 250px;
+   user-select: none;
+   cursor: pointer;
+   text-align: center;
+   -webkit-tap-highlight-color: transparent;
+  }`;
 
   constructor() {
     document.addEventListener('DOMContentLoaded', (event) => {
